6a86b6e09401a2cceced6d03a22c7902fc716a90
Stanislav <snikolenko@flo.team>
2022-03-12 18:35:40 +0300

    refactor: create fucntion for login
    create a separate function for user login
    
    MODULE-1-PART-3-gallery

--- utilities/dataSubmitting.js
+++ utilities/dataSubmitting.js
@@ -1,7 +1,6 @@
 import { validateEmail } from "./emailValidation.js";
 import { validatePassword } from "./passwordValidation.js";
-import { BASE_URL } from "../urls/urls.js";
-import { getPageNumberFromUrl } from "./urlManipulation.js";
+import { loginUser } from "./dataSubmitting/login.js";
 
 export const submitUserData = () => {
     return async (event) => {
@@ -14,43 +13,7 @@ export const submitUserData = () => {
         const validatedPassword = validatePassword(password);
 
         if (validatedEmail.isValid && validatedPassword.isValid) {
-            try {
-                const user = {
-                    email: email,
-                    password: password
-                };
-
-                const url = `${BASE_URL}/login`;
-
-                const response = await fetch(url, {
-                    method: 'POST',
-                    body: JSON.stringify(user)
-                });
-
-                const result = await response.json();
-
-                if (response.ok) {
-                    localStorage.setItem('token', result.token);
-
-                    const oneMinuteInMs = 60000;
-                    const tenMinutes = oneMinuteInMs * 10;
-
-                    const tokenExpiredTime = Date.now() + tenMinutes;
-                    localStorage.setItem('tokenExpiredTime', tokenExpiredTime);
-
-                    let pageNumber = getPageNumberFromUrl();
-
-                    if (!pageNumber) {
-                        pageNumber = 1;
-                    }
-
-                    window.location.href = `/../gallery/gallery.html?page=${pageNumber}`;
-                } else {
-                    alert(result.errorMessage);
-                }
-            } catch(e) {
-                console.log(e);
-            }
+            await loginUser(email, password);
         }
     }
 };
