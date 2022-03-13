bb3403084b54e486a4866c0ee91353bbd0fa03e9
Stanislav <snikolenko@flo.team>
2022-03-13 22:15:25 +0300

    refactor: create a function for rendering a pages list
    create a function for rendering a pages list
    
    MODULE-1-PART-3-gallery

--- utilities/render/render.js
+++ utilities/render/render.js
@@ -1,6 +1,11 @@
-import { wrapUrlsInHtml } from "../htmlWrapping.js";
+import { wrapNumbersInHtml, wrapUrlsInHtml } from "../htmlWrapping.js";
 
 export function renderImages(imagesUrls) {
     const images = document.getElementById('images');
     images.innerHTML = wrapUrlsInHtml(imagesUrls);
+}
+
+export function renderPagesList(totalNumberOfPages) {
+    const pages = document.getElementById('pages');
+    pages.innerHTML = wrapNumbersInHtml(totalNumberOfPages);
 }
